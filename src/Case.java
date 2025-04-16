92a82c254c4d947471597dc78b605f092a233fa3
Camille-Astrid <camilleastrid.cr@gmail.com>
2025-04-16 16:12:06 +0200

    Tentative de construction de l'interface graphique

--- src/Case.java
+++ src/Case.java
@@ -1,7 +1,11 @@
 public enum Case {
     // Enumération des différentes cases possibles correspondant à l'alphabet
-    A("A"), B("B"), C("C"), D("D"), E("E"), F("F"), G("G"), H("H"), I("I"), J("J"), K("K"), L("L"), M("M"), N("N"), O("O"), P("P"), Q("Q"), R("R"), S("S"), T("T"), U("U"), V("V"), W("W"), X("X"), Y("Y"), Z("Z"),
-    VIDE(" ");
+    A("A"), B("B"), C("C"), D("D"), E("E"),
+    F("F"), G("G"), H("H"), I("I"), J("J"),
+    K("K"), L("L"), M("M"), N("N"), O("O"),
+    P("P"), Q("Q"), R("R"), S("S"), T("T"),
+    U("U"), V("V"), W("W"), X("X"), Y("Y"),
+    Z("Z"), VIDE(" ");
 
     private String symbole;
 
--- src/InterfaceMotus.java
+++ src/InterfaceMotus.java
@@ -1,7 +1,6 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import java.awt.*;
 
 public class InterfaceMotus {
     JFrame fenetre = new JFrame("Motus Master");
@@ -10,12 +9,42 @@ public class InterfaceMotus {
         Dimension currentScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
         fenetre.setSize(currentScreenSize);
         fenetre.setUndecorated(false);
-        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetre.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         fenetre.setVisible(true);
     }
 
+    public void AffichageGrille(int nb_letters, boolean terminal) {
+        if (terminal) {
+            System.out.println("--".repeat(nb_letters));
+            for (int i = 0; i < 5; i++) {
+                System.out.print("| ");
+                for (int j = 0; j < nb_letters; j++) {
+                    System.out.print(Plateau.grille[j][i] + " | ");
+                }
+                System.out.println();
+                System.out.println("--".repeat(nb_letters));
+            }
+        } else {
+            // Affichage sur interface graphique
+            JPanel panel = new JPanel();
+            GridLayout gridLayout = new GridLayout(5, nb_letters);
+            panel.setLayout(gridLayout);
+            for (int i = 0; i < nb_letters*5; i++) {
+                JLabel label = new JLabel(Case.VIDE.getSymbole());
+                label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                panel.add(label);
+            }
+            //ajoute les bordures de la grille avec setBorder
+            panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            fenetre.add(panel);
+            fenetre.setVisible(true);
+        }
+    }
+
     public static void main(String[] args) {
-        InterfaceMotus affichage = new InterfaceMotus();
+        // Exemple d'utilisation
+        InterfaceMotus interfaceMotus = new InterfaceMotus();
+        interfaceMotus.AffichageGrille(5, false);
     }
 
 }
