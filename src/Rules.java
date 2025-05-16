5f2f896cd80d737b8191e1914c71976f897efae7
Camille-Astrid <camilleastrid.cr@gmail.com>
2025-05-16 17:35:17 +0200

    ADD_function_html-reading

--- src/Rules.java
+++ src/Rules.java
@@ -16,15 +16,32 @@ public class Rules extends Initialize{
         JButton retour = new JButton("Retour");
 
         //Creation des layouts
-        JTextArea textArea = new JTextArea(text);
-        textArea.setLineWrap(true); // Permet de faire un retour à la ligne automatique
-        textArea.setWrapStyleWord(true); // Permet de ne pas couper les mots
-        textArea.setMargin(new Insets(20,20,20,200)); // Ajout d'une marge autour du texte
-        textArea.setBackground(motusColor);
-        textArea.setForeground(Color.white);
-        textArea.setFont(new Font("Arial", Font.PLAIN, 20));
-        textArea.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        JTextPane textPane = new JTextPane(){
+            @Override
+            public boolean getScrollableTracksViewportWidth() {
+                return getUI().getPreferredSize(this).width <= getParent().getSize().width;
+            }
+        };
+        textPane.setContentType("text/html");
+        textPane.setText(text);
+        textPane.setEditable(false);
+        textPane.setBackground(new Color(0x101044));
+        textPane.setForeground(new Color(0xFF33FF));
+        textPane.setFont(new Font("Consolas", Font.BOLD, 16));
+        textPane.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        
+
+        //JTextArea textArea = new JTextArea(text);
+        //textArea.setLineWrap(true); // Permet de faire un retour à la ligne automatique
+        //textArea.setWrapStyleWord(true); // Permet de ne pas couper les mots
+        //textArea.setMargin(new Insets(20,20,20,200)); // Ajout d'une marge autour du texte
+        //textArea.setBackground(motusColor);
+        //textArea.setForeground(Color.white);
+        //textArea.setFont(new Font("Arial", Font.PLAIN, 20));
+        //textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textPane, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setBorder(null);
         scrollPane.getVerticalScrollBar().setPreferredSize(new Dimension(0, 0));
         JPanel rightPanel = new JPanel(new BorderLayout());
